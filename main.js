fd939fcb15e7f65b53824192ac2e9c2fbd15cd3a
Tom Rowan Brooks <DocWeaver@Toms-MacBook-Air.local>
2020-12-26 13:01:08 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const messages = ["Christmas is great", "elf on the shelf", "London Bridge is falling donw", " random message 4"]
+
+const randomArrayGenerator = (array) => {
+    return Math.floor(Math.random(0,1)*array.length)
+}
+
+console.log(messages[randomArrayGenerator(messages)])
